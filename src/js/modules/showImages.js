6fdccb14616ea071db64c84ca94e21e765ab6c37
AdelRomeo <11cage11@mail.ru>
2021-04-07 23:21:32 +0700

    revisions show images

--- src/js/modules/showImages.js
+++ src/js/modules/showImages.js
@@ -24,16 +24,22 @@ const showImages = () => {
     if (e.target.classList.contains('preview')) {
       //показываем popup
       imgPopup.style.display = 'flex';
+      //запрещаем скролл страницы
+      document.body.style.overflow = 'hidden';
       //получаем путь к картинке
       const src = e.target.parentNode.getAttribute('href');
       //добавляем путь к картинке
       bigImg.setAttribute('src', src);
+      bigImg.style.maxWidth = '100%';
+      bigImg.style.maxHeight = '100%';
     }
 
     //если кликнули именно по диву с классом popup (не по картинке)
     if (e.target.matches('div.popup')) {
       //скрываем popup
       imgPopup.style.display = 'none';
+      //разрешаем скролл страницы
+      document.body.style.overflow = '';
     }
 
   });
